11b80bffa04e1a3af2ed9e6677bae5d686de3e64
Ali Churcher <churcher.ali@gmail.com>
2020-01-12 22:51:28 +1300

    Add title and author styling as per Medium site

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { card } from './Post.module.css';
+import { card, author, title } from './Post.module.css';
 
 function Post(props) {
   return (
     <article className={card}>
-      <div>{props.title}</div>
-      <div>{props.author}</div>
+      <div className={author}>{props.author}</div>
+      <div className={title}>{props.title}</div>
       <div>{props.slug}</div>
       <br></br>
     </article>

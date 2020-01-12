3a863b78a12240727b88ba64b046a30c6959e218
Ali Churcher <churcher.ali@gmail.com>
2020-01-13 00:10:19 +1300

    add notes to readme

--- src/components/trending-posts/TrendingPosts.js
+++ src/components/trending-posts/TrendingPosts.js
@@ -12,7 +12,6 @@ class TrendingPosts extends React.Component {
     fetch('http://localhost:3000/posts')
       .then((result) => result.json())
       .then((posts) => {
-        console.log(posts);
         this.setState({ posts: posts });
       });
   }

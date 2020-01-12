7aec763dd0dfcb14afee29ddce031cdb244a0d7a
Ali Churcher <churcher.ali@gmail.com>
2020-01-12 21:50:34 +1300

    Hit posts API and prints response to console

--- src/components/trending-articles/TrendingArticles.js
+++ src/components/trending-articles/TrendingArticles.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Article from '../article/Article.js'
 
-function TrendingArticles() {
-  return (
-    <main>
-      <div>Here are the trending articles:</div>
-      <Article></Article>
-      <Article></Article>
-      <Article></Article>
-    </main>
-  );
+class TrendingArticles extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/posts')
+      .then((result) => result.json())
+      .then((posts) => console.log(posts));
+  }
+
+  render() {
+    return (
+      <main>
+        <div>Here are the trending articles:</div>
+        <Article></Article>
+        <Article></Article>
+        <Article></Article>
+      </main>
+    );
+  }
 }
 
 export default TrendingArticles;

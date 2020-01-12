70ab6325fcc3444dc7603bc80b234c7a9c83e601
Ali Churcher <churcher.ali@gmail.com>
2020-01-12 21:55:09 +1300

    Add state to hold post data

--- src/components/trending-articles/TrendingArticles.js
+++ src/components/trending-articles/TrendingArticles.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Article from '../article/Article.js'
 
 class TrendingArticles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { posts: [] };
+  }
 
   componentDidMount() {
     fetch('http://localhost:3000/posts')
       .then((result) => result.json())
-      .then((posts) => console.log(posts));
+      .then((posts) => {
+        console.log(posts);
+        this.setState({ posts: posts });
+      });
   }
 
   render() {

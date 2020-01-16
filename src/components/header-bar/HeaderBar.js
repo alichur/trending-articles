7ff738d1377b4b1e0e509079a1533efb995b26e9
Ali Churcher <churcher.ali@gmail.com>
2020-01-16 23:21:01 +1300

    Add header buttons and supporting media query

--- src/components/header-bar/HeaderBar.js
+++ src/components/header-bar/HeaderBar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import MediumLogoSvg from './MediumLogoSmall.js';
-import { container, button } from './HeaderBar.module.css';
+import SearchIcon from './SearchIcon.js';
+import { container, button, actions, search, member, signIn } from './HeaderBar.module.css';
 
+// This header is for CSS demo purposes only and the items have no function.
 
 function HeaderBar() {
     return <header className={container}>
         <MediumLogoSvg />
-        <button className={button}>Get started</button>
-
+        <div className={actions}>
+            <SearchIcon className={search} />
+            <a className={member} href="#">Become a member</a>
+            <a className={signIn} href="#">Sign in</a>
+            <button className={button}>Get started</button>
+        </div>
     </header >
 }
 
---
+++ src/components/header-bar/SearchIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchIcon = props => (
+  <svg width="25" height="25">
+    <path opacity="0.54" d="M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z">
+    </path>
+  </svg>
+
+)
+
+export default SearchIcon
